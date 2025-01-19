7c7210bbbc5e52da19abe4de595f893c40051d7d
HoangNguyenk8 <nguyenhoanggianamk8@gmail.com>
2025-01-19 18:23:33 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 2753915549 or game.PlaceId == 2753915549 or game.PlaceId == 2753915549 then
+  loadstring(game:HtppGet("https://raw.githubusercontent.com/HoangNguyenk8/Scripts/refs/heads/main/BF-Main.lua"))()
+elseif game.PlaceId == 4520749081 or game.PlaceId == 4520749081 or game.PlaceId == 4520749081 then
+loadstring(game:HtppGet("https://raw.githubusercontent.com/HoangNguyenk8/Scripts/refs/heads/main/KL-Main.lua"))()
+end
