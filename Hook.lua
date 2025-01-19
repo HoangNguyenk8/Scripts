68c21914ac2abfb8e7da98a85ca30691f51176df
HoangNguyenk8 <nguyenhoanggianamk8@gmail.com>
2025-01-19 18:11:38 +0700

    Create Hook.lua

---
+++ Hook.lua
@@ -0,0 +1,2 @@
+hookfunction(require(game.ReplicatedStorage.Effect.Container.Death), function() return nil end)
+hookfunction(require(game.ReplicatedStorage.Effect.Container.Respawn), function() return nil end)
