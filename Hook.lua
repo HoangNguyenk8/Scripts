fce1f414d77458a1414d9392e9d868e3c6ede956
HoangNguyenk8 <nguyenhoanggianamk8@gmail.com>
2025-01-28 11:47:39 +0700

    Update Hook.lua

--- Hook.lua
+++ Hook.lua
@@ -1,3 +1,7 @@
 hookfunction(require(game.ReplicatedStorage.Effect.Container.Death), function() return nil end)
 hookfunction(require(game.ReplicatedStorage.Effect.Container.Respawn), function() return nil end)
+hookfunction(require(game.ReplicatedStorage:WaitForChild("GuideModule")).ChangeDisplayedNPC, function() return nil end)
 require(game.ReplicatedStorage.Util.CameraShaker):Stop()
+if workspace:FindFirstChild("Rocks") then workspace:FindFirstChild("Rocks"):Destroy() end
+if workspace._WorldOrigin["Foam;"] then workspace._WorldOrigin["Foam;"]:Destroy() end
+setfpscap(60)
