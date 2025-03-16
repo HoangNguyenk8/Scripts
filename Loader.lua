a76d2240a0e2c711fe1fcbb614b6289487ba6a83
HoangNguyenk8 <nguyenhoanggianamk8@gmail.com>
2025-03-16 03:38:27 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,12 @@
-if game.PlaceId == 2753915549 or game.PlaceId == 2753915549 or game.PlaceId == 2753915549 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/HoangNguyenk8/Scripts/refs/heads/main/BF-Main.lua"))()
-elseif game.PlaceId == 4520749081 or game.PlaceId == 4520749081 or game.PlaceId == 4520749081 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HoangNguyenk8/Scripts/refs/heads/main/KL-Main.lua"))()
+function LoaderScript(main)
+  if main == 2753915549 or main == 4442272183 or main == 7449423635 then
+    return https://raw.githubusercontent.com/HoangNguyenk8/Scripts/refs/heads/main/BF-Main.lua
+  else
+    return "Kick"
+  end
+end
+if LoaderScript(game.PlaceId) ~= "Kick" then
+  loadstring(game:HttpGet(LoaderScript(game.PlaceId)))()
+else
+  game.Players.LocalPlayer:Kick("Zinner Hub\n Not Support This Place")
 end
